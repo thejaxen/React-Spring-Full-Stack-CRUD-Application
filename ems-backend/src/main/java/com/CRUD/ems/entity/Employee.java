48677bcb08fab4852d0478938420bd514001b0b3
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-07-10 18:36:36 +0300

    Table and Entity
    Employee table is finished and entity class is done.

--- ems-backend/src/main/java/com/CRUD/ems/entity/Employee.java
+++ ems-backend/src/main/java/com/CRUD/ems/entity/Employee.java
@@ -1,13 +1,30 @@
 package com.CRUD.ems.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter /*for reading the values of variables*/
+@Setter /*for updating the values of variables*/
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
     private String email;
 }
